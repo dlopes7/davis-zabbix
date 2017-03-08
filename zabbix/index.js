0f42429d8d338abe5f41e0610d00eb405af263ff
David Lopes <davidribeirolopes@gmail.com>
2017-03-08 16:11:31 -0300

    Adding a README and some zabbix api tests

--- zabbix/index.js
+++ zabbix/index.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const util = require('util')
+const zabbixAPI = require('zabbix-api');
+
+
 class Zabbix {
     constructor(davis, options) {
         this.davis = davis;
         this.options = options;
+        this.zbx = new zabbixAPI(process.env.ZABBIX_USER, process.env.ZABBIX_PASSWORD, 'http://zabbix-server01.dc.nova/zabbix/api_jsonrpc.php');
+
+        console.log(this.zbx)
 
         this.intents = {
             zabbix: {
@@ -14,6 +21,7 @@ class Zabbix {
                 ],
                 phrases: [
                     "What happened yesterday in Zabbix?",
+                    "What are the active triggers in Zabbix?",
                 ],
                 lifecycleEvents: [
                     'gatherData',
@@ -31,13 +39,32 @@ class Zabbix {
     }
 
     gatherData(exchange) {
-        //const tense = this.davis.utils.getTense(exchange);
-        console.log(exchange);
+
+        const isTriggers = /[tT]rigger/i.test(exchange.getRawRequest());
+        console.log(isTriggers)
+
+
+        this.zbx.request('trigger.get', {
+            filter: {
+                value: 1
+            },
+            selectHosts: 'extend'
+        }, function(err, res) {
+            if (err) {
+                console.log(err.message);
+            } else {
+                // console.log(res);
+            }
+        });
+
+        const tense = this.davis.utils.getTense(exchange);
+        console.log(util.inspect(exchange.getRawRequest(), false, null))
+        console.log(util.inspect(tense));
     }
 
     respond(exchange) {
         //const tense = this.davis.utils.getTense(exchange);
-        console.log(exchange);
+        //console.log(exchange);
         exchange.response('Porra nenhuma irmão!');
 
     }
